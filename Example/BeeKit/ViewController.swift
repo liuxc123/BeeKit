bd957301c5380a2c52542194e3dd6ec95ea7aafe
liuxc <lxc_work@126.com>
2020-12-31 14:01:45 +0800

    Initial commit

---
+++ Example/BeeKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BeeKit
+//
+//  Created by liuxc123 on 12/31/2020.
+//  Copyright (c) 2020 liuxc123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
