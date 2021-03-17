6d7d7d08c71e003e34234f8687654d4475299239
liuxc <lxc_work@126.com>
2021-03-17 10:09:35 +0800

    update

--- Example/BeeKit/Config/AppEnv.swift
+++ Example/BeeKit/Config/AppEnv.swift
@@ -14,20 +14,10 @@ public class Envs: EnvsProtocol {
 
     public enum EnvsMode: Int {
         case dev        = 0
-        case test       = 1
-        case product    = 2
     }
 
     /// 环境变量
     public static var envMode: EnvsMode {
-//        #if DEBUG
-//        if let env = UserDefaults.standard.value(forKey: "app_network_env") as? Int {
-//            return EnvsMode(rawValue: env) ?? .dev
-//        }
-//        return .test
-//        #else
-//        return .product
-//        #endif
         return .dev
     }
 
@@ -41,17 +31,13 @@ public extension EnvsProtocol {
 
     static var base_url: String {
         switch Envs.envMode {
-        case .dev: return "http://dev.api.fleet.webus.vip:12321/fleet/api/v1/mobile"
-        case .test: return "http://test.api.fleet.webus.vip:12321/fleet/api/v1/mobile"
-        case .product: return "http://call.fleet.ubus.vip/fleet/api/v1/mobile"
+        case .dev: return "http://******"
         }
     }
 
     static var h5_url: String {
         switch Envs.envMode {
-        case .dev: return "http://dev.app.fleeth5.webus.vip:12321"
-        case .test: return "http://test.app.fleeth5.webus.vip:12321"
-        case .product: return "http://fleeth5.ubus.vip"
+        case .dev: return "http://******"
         }
     }
 
